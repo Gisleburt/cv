d7d06a082a6fd892f9a3c986837fd61472c79fe8
Daniel Mason <daniel@danielmason.com>
2025-07-16 13:56:54 +0100

    feat: personal statement macro added

--- src/content/personal_statement.rs
+++ src/content/personal_statement.rs
@@ -2,40 +2,52 @@ use crate::components::*;
 use dioxus::prelude::*;
 use indoc::indoc;
 
+macro_rules! statement {
+    ( $( { $($tokens:tt)+ } )+ ) => {
+        rsx!(
+            $(
+                p { $({ statement_helper!($tokens) })+ }
+            )+
+        )
+    };
+}
+
+macro_rules! statement_helper {
+    ($normal:literal) => {
+        $normal
+    };
+    ([$emphasis:literal]) => {
+        rsx!(em { $emphasis })
+    };
+}
+
 #[component]
 pub fn PersonalStatement() -> Element {
-    let statements = [
-        rsx!(
-            {"Throughout my many years in technical leadership, I've always believed that"}
-            em { " the real 10x engineers are those helping everyone around them achieve more " }
-            {indoc! {"
-                . This has been the north star of my leadership style; show my team what supporting
-                each other looks like, give them the tools they need, and help them grow, and they
-                will always deliver to their best ability.
-            "}}
-        ),
-        rsx!({
-            indoc! {"
-                Hire me to build strong teams of people who deliver value through understanding
-                the business requirements and the application of engineering excellence. I will
-                achieve this through building confidence, trust and a culture of support and
-                self-development.
-            "}
-        }),
-        rsx!({
-            indoc! {"
-                Despite my success, self-development remains my goal too. I strive to keep learning
-                how to be a better manager, particularly continuing to learn how to best align with
-                other teams and work on larger projects.
-            "}
-        }),
-    ];
+    let statements = statement!(
+        {
+            "Throughout my many years in technical leadership, I've always believed that"
+            [" the real 10x engineers are those helping everyone around them achieve more"]
+            ". This has been the north star of my leadership style; show my team what  supporting "
+            "each other looks like, give them the tools they need, and help them grow, and they "
+            "will always deliver to their best ability."
+        }
+
+        {
+            "Hire me to build strong teams of people who deliver value through understanding the "
+            "business requirements and the application of engineering excellence. I will achieve "
+            "this through building confidence, trust and a culture of support and self-development."
+        }
+
+        {
+             "Despite my success, self-development remains my goal too. I strive to keep learning "
+             "how to be a better manager, particularly continuing to learn how to best align with "
+             "other teams and work on larger projects."
+        }
+    );
 
     rsx!(
         Section { title: "Personal Statement",
-            for statement in statements {
-               p {{ statement }}
-            }
+            {statements}
         }
     )
 }
